0a35cd349061466909e942a307f2d61f03f26451
reverserob <robertarandazzo@outlook.com>
2019-08-24 15:54:22 +0200

    Updates

---
+++ precache-manifest.a26b554a771c3b9853b3551feb8871fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f9c784dcc51815498afee23c64e67d",
+    "url": "/cats-cards-react/index.html"
+  },
+  {
+    "revision": "4be6a0738dd4891f1865",
+    "url": "/cats-cards-react/static/css/main.c52e20b5.chunk.css"
+  },
+  {
+    "revision": "9a15a2f8a7418d44e78e",
+    "url": "/cats-cards-react/static/js/2.df0b5369.chunk.js"
+  },
+  {
+    "revision": "4be6a0738dd4891f1865",
+    "url": "/cats-cards-react/static/js/main.6d5df6a5.chunk.js"
+  },
+  {
+    "revision": "1f327d23b22d6c6df03d",
+    "url": "/cats-cards-react/static/js/runtime~main.5298829d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cats-cards-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
